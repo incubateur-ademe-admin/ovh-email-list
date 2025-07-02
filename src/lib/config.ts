26aa43cde7c6df2296101d5ff663c35e2da6ad33
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-07-02 23:27:12 +0200

    chore: add login ttl via cookies

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,3 +4,4 @@ export const OVH_APP_SECRET = process.env.OVH_APP_SECRET || "";
 export const OVH_CONSUMER_KEY = process.env.OVH_CONSUMER_KEY || "";
 export const BASIC_AUTH_USER = process.env.BASIC_AUTH_USER || "admin";
 export const BASIC_AUTH_PASS = process.env.BASIC_AUTH_PASS;
+export const APP_DOMAIN = process.env.APP_DOMAIN || "localhost";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,43 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { BASIC_AUTH_PASS, BASIC_AUTH_USER } from './lib/config';
+import { BASIC_AUTH_PASS, BASIC_AUTH_USER, APP_DOMAIN } from './lib/config';
+
+const COOKIE_NAME = 'basic-auth';
+const COOKIE_MAX_AGE = 5 * 60; // 5 minutes en secondes
 
 export function middleware(req: NextRequest) {
+  const cookie = req.cookies.get('basic-auth');
   const authHeader = req.headers.get('authorization');
 
-  if (authHeader) {
-    const base64 = authHeader.split(' ')[1];
-    const [user, pass] = atob(base64).split(':');
-
-    if (user === BASIC_AUTH_USER && pass === BASIC_AUTH_PASS) {
-      return NextResponse.next(); // OK
-    }
-  }
-
-  return new NextResponse('Auth required', {
+  const resAuth = new NextResponse('Auth required', {
     status: 401,
     headers: {
       'WWW-Authenticate': 'Basic realm="Protected Area"',
     },
   });
+
+  // Si le cookie est présent → ne PAS regarder le header
+  if (cookie?.value !== '1') {
+    resAuth.cookies.set({
+      name: COOKIE_NAME,
+      value: '1',
+      maxAge: COOKIE_MAX_AGE,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'lax',
+      domain: process.env.NODE_ENV === 'production' ? APP_DOMAIN : undefined, // Remplacez par votre domaine de production
+    });
+    return resAuth;
+  } else if (authHeader){
+    const base64 = authHeader.split(' ')[1];
+    const [user, pass] = atob(base64).split(':');
+    if (user === BASIC_AUTH_USER && pass === BASIC_AUTH_PASS) {
+      return NextResponse.next();
+    }
+  }
+
+  return resAuth;
 }
 
 export const config = {
